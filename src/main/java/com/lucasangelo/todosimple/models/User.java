1a4493614fde321281f8516da18c2e84cc6c6d99
guidev1911 <guilhermereal1911@gmail.com>
2024-05-20 22:16:36 -0300

    Create user model

---
+++ src/main/java/com/lucasangelo/todosimple/models/User.java
@@ -0,0 +1,46 @@
+package com.lucasangelo.todosimple.models;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.springframework.web.servlet.FlashMapManager;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "user")
+
+public class User {
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true) 
+    private Long id;
+
+    @Column(name = "username", length = 100, nullable = false, unique = true)
+    @Size(min = 2, max = 100)
+    @NotBlank
+    private String username;
+
+    @Column(name = "password", length = 60, nullable = false)
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Size(min = 8, max = 60)
+    @NotBlank
+    private String password;
+}
