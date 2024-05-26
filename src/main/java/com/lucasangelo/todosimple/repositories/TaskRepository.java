cb400ae569dd18bcd217ef9676d4de37c7ff9af3
guidev1911 <guilhermereal1911@gmail.com>
2024-05-26 13:39:00 -0300

    add task projection

--- src/main/java/com/lucasangelo/todosimple/repositories/TaskRepository.java
+++ src/main/java/com/lucasangelo/todosimple/repositories/TaskRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.lucasangelo.todosimple.models.Task;
+import com.lucasangelo.todosimple.models.projection.TaskProjection;
 
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long>{
-    List<Task> findByUser_Id (long id);
+    List<TaskProjection> findByUser_Id (long id);
 }
